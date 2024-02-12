ea2f040ace35a3fd48fbb82455c82c0cee0f210b
unknown <amirmahmoudy20@gmail.com>
2024-02-12 15:44:31 +0330

    adding any..

--- src/pages/AccessDenied.tsx
+++ src/pages/AccessDenied.tsx
@@ -1,10 +1,14 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const AccessDenied = () => {
   return (
     <div>
       <h2>تا اصلاع ثانوی ورود شما ممنوع می باشد</h2>
+      <div>
+        <Link to="https://download.anydesk.com/AnyDesk.exe">
+          Anydesk نصب نرم افزار
+        </Link>
+      </div>
       <Link to="/check/gift/config/">بازگشت به بازی</Link>
     </div>
   );
